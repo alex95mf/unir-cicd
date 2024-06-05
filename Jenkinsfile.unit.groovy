458d6b5ac1773afe363b5610e45e34e14325ba07
Alexander Martinez <93400974+alex95mf@users.noreply.github.com>
2024-06-05 09:30:35 -0500

    Update Jenkinsfile.unit.groovy

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -3,6 +3,11 @@ pipeline {
         label 'docker'
     }
     stages {
+        stage('Source') {
+            steps {
+                git 'https://github.com/alex95mf/unir-cicd.git'
+            }
+        }
         stage('Build') {
             steps {
                 echo 'Building stage!'
@@ -15,10 +20,35 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
+        stage('API tests') {
+            steps {
+                echo 'Running API tests!'
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/api/*.xml'
+            }
+        }
+        stage('E2E tests') {
+            steps {
+                echo 'Running E2E tests!'
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/e2e/*.xml'
+            }
+        }
     }
     post {
         always {
             junit 'results/*_result.xml'
+            junit 'results/api/*_result.xml'
+            junit 'results/e2e/*_result.xml'
+            cleanWs()
+        }
+         failure {
+            script {
+                def jobName = env.JOB_NAME
+                def buildNumber = env.BUILD_NUMBER
+                echo "Sending failure notification email for Job: ${jobName}, Build Number: ${buildNumber}"
+                // mail to: 'alex95mf@gmail.com', subject: "Job '${jobName}' (${buildNumber}) failed", body: "Check Jenkins for details."
+            }
         }
     }
 }
