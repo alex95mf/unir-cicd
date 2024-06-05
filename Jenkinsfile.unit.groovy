bfd4ff12cea4e82eb498dc2ba2cd7c7946fc4817
Alexander Martinez <93400974+alex95mf@users.noreply.github.com>
2024-06-05 12:44:21 -0500

    Update Jenkinsfile.unit.groovy final

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -22,22 +22,22 @@ pipeline {
             steps {
                 echo 'Running API tests!'
                 sh 'make test-api'
-                archiveArtifacts artifacts: 'results/api/*.xml'
+                archiveArtifacts artifacts: 'results/*.xml'
             }
         }
         stage('E2E tests') {
             steps {
                 echo 'Running E2E tests!'
                 sh 'make test-e2e'
-                archiveArtifacts artifacts: 'results/e2e/*.xml'
+                archiveArtifacts artifacts: 'results/*.xml'
             }
         }
     }
     post {
         always {
             junit 'results/*_result.xml'
-            junit 'results/api/*_result.xml'
-            junit 'results/e2e/*_result.xml'
+            junit 'results/*_result.xml'
+            junit 'results/*_result.xml'
             cleanWs()
         }
         failure {
@@ -45,7 +45,9 @@ pipeline {
                 def jobName = env.JOB_NAME
                 def buildNumber = env.BUILD_NUMBER
                 echo "Sending failure notification email for Job: ${jobName}, Build Number: ${buildNumber}"
-                // mail to: 'alex95mf@gmail.com', subject: "Job '${jobName}' (${buildNumber}) failed", body: "Check Jenkins for details."
+                emailext body: "Error encontrado en el pipeline",
+                         subject: "Error en el pipeline",
+                         to: "alex95mf@gmail.com"
             }
         }
     }
